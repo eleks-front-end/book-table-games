40d13e30ae8f9beca7fb2d50745e7d0983b328f1
domovoj <domovoj1@gmail.com>
2017-05-26 18:01:31 +0300

    add reserve page

--- app/components/Layout/Base.jsx
+++ app/components/Layout/Base.jsx
@@ -8,6 +8,7 @@ import Footer from './Footer';
 import { Route, Switch } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
 import Tennis from '../Tennis/Tennis';
+import ReserveTennis from '../ReserveTennis/ReserveTennis';
 import Billiard from '../Billiard/Billard';
 
 class Base extends React.Component {
@@ -50,8 +51,8 @@ class Base extends React.Component {
                 >
                     <Switch>
                         <Route exact path="/" component={Dashboard}/>
-                        <Route path="/tennis" component={Tennis}/>
-                        <Route path="/tennis/reserve" component={Tennis}/>
+                        <Route exact path="/tennis" component={Tennis}/>
+                        <Route path="/tennis/reserve" component={ReserveTennis}/>
                         <Route path="/billiard" component={Billiard}/>
                     </Switch>
 
---
+++ app/components/ReserveTennis/ReserveTennis.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import ContentWrapper from '../Layout/ContentWrapper';
+import { Button, Col, OverlayTrigger, Row, Table, Tooltip } from 'react-bootstrap';
+import styles from './reserve-tennis.style.scss';
+import cssModules from 'react-css-modules';
+import tennisReserveRun from './ReserveTennis.run';
+import TimePicker from 'rc-time-picker';
+
+class TennisReserveView extends React.Component {
+    componentDidMount () {
+        tennisReserveRun();
+    }
+
+    render () {
+        const tooltipDelete = (
+            <Tooltip id="tooltip">Cancel game</Tooltip>
+        );
+        const tooltipDeleteMe = (
+            <Tooltip id="tooltip">Cancel me from game</Tooltip>
+        );
+
+        return (
+            <ContentWrapper>
+                <Row>
+                    <Col xs={12}>
+                        <h1 className="text-thin">Reserve Table tennis</h1>
+                        <Row>
+                            <div className="col-sm-4">
+                                <TimePicker
+                                    showSecond={false}
+                                    className="xxx"
+                                    placeholder="From"
+                                    // onChange={onChange}
+                                    // format={format}
+                                    use12Hours
+                                />
+
+                                <TimePicker
+                                    showSecond={false}
+                                    className="xxx"
+                                    placeholder="To"
+                                    // onChange={onChange}
+                                    // format={format}
+                                    use12Hours
+                                />
+                            </div>
+                            <div className="form-group col-sm-3">
+                                { /*  SELECT2 */ }
+                                <select id="select2-1" className="form-control" data-placeholder="Search by user">
+                                    <option></option>
+                                    <option value="1">Participant 1</option>
+                                    <option value="2">Participant 2</option>
+                                    <option value="3">Participant 3</option>
+                                    <option value="4">Participant 4</option>
+                                </select>
+                            </div>
+                        </Row>
+                        <Button bsStyle="primary">Reserve</Button>
+
+                        <h2 className="text-thin">My reserved games</h2>
+
+                        <Table responsive>
+                            <thead>
+                            <tr>
+                                <th width="100">Time</th>
+                                <th>Participants</th>
+                                <th width="30"></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>9:00-10:00</td>
+                                <td>
+                                    <a href="">Participant 1</a>,&nbsp;
+                                    <a href="">Participant 2</a>,&nbsp;
+                                    <a href=""><b>Me</b></a>
+                                </td>
+                                <td>
+                                    <OverlayTrigger placement="top" overlay={tooltipDeleteMe}>
+                                        <Button bsStyle="danger" className="p-sm">
+                                            <em className="icon-trash"/>
+                                        </Button>
+                                    </OverlayTrigger>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>9:00-10:00</td>
+                                <td>
+                                    <a href="">Participant 1</a>,&nbsp;
+                                    <a href=""><b>Me</b></a>
+                                </td>
+                                <td>
+                                    <OverlayTrigger placement="top" overlay={tooltipDelete}>
+                                        <Button bsStyle="danger" className="p-sm">
+                                            <em className="icon-trash"/>
+                                        </Button>
+                                    </OverlayTrigger>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+            </ContentWrapper>
+        );
+    }
+}
+
+export default cssModules(TennisReserveView, styles);
